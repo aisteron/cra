5103c72ab3c1f281a150b687528638af191202bb
unknown <timotheus@list.ru>
2019-05-09 22:15:08 +0300

    first commit

---
+++ src/components/app/app.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+
+import AppHeader from '../app-header';
+import SearchPanel from '../search-panel';
+import TodoList from '../todo-list';
+import ItemStatusFilter from '../item-status-filter';
+import ItemAddForm from '../item-add-form';
+
+import './app.css';
+
+export default class extends Component {
+
+    state = {
+        todoData : [
+            {label: 'Drink Coffee', important: false, id: 1},
+            {label: 'Make Awesome App', important: true, id: 2},
+            {label: 'Have a lunch', important: false, id: 3}
+        ]
+    };
+
+    deleteItem = (id) => {
+        this.setState(({todoData}) => {
+            const idx = todoData.findIndex((el) => el.id === id);
+            //todoData.splice(idx, 1);
+
+            const before = todoData.slice(0, idx);
+            const after = todoData.slice(idx + 1);
+
+
+            const newArray = [...before, ...after];
+            return {
+                todoData: newArray
+            }
+        })
+    };
+
+    render ()
+    {
+        return (
+            <div className="todo-app">
+                <AppHeader toDo={1} done={3} />
+                <div className="top-panel d-flex">
+                    <SearchPanel />
+                    <ItemStatusFilter />
+                </div>
+
+                <TodoList
+                    todos={this.state.todoData}
+                    onDeleted={ this.deleteItem}
+                />
+            </div>
+        );
+    }
+
+
+};
